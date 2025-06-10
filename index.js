2c0577c20121ec88a0a9fe21cf858e58065889eb
Jeferth Crisostomo <jeferth.crisostomo@gmail.com>
2025-06-10 12:07:07 -0300

    feat: 🎸 add entrypoints

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const SeederFactory = require("./lib/seeder-factory");
+const SeederConfig = require("./lib/seeder-config");
+const DataProcessor = require("./lib/data-processor");
+
+module.exports = {
+  SeederFactory,
+  SeederConfig,
+  DataProcessor,
+};
